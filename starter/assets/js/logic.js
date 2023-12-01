b99d0f7d212e27828c3dd64870048b020416f10e
Liam Cottrell <lemcottrell@gmail.com>
2023-12-01 19:59:33 +0000

    make first getquestion function

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,8 @@
+
+document.getElementById("start").addEventListener("click", getQuestion);
+
+function getQuestion() {
+    document.getElementById("start-screen").setAttribute("class", "hide");
+    document.getElementById("questions").setAttribute("class", "")
+}
+
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        title: "question1",
+        choices: ["a1", "a2", "a3"],
+        correctAnswer: "correct",
+    },
+    {
+        title: "question2",
+        choices: ["a1", "a2", "a3"],
+        correctAnswer: "correct",
+    },
+    {
+        title: "question3",
+        choices: ["a1", "a2", "a3"],
+        correctAnswer: "correct",
+    },
+]
+
+
