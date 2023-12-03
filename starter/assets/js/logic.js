84f4a0a2c7c83122a8868ce96ffabe51a1cecd7d
Liam Cottrell <lemcottrell@gmail.com>
2023-12-03 19:52:18 +0000

    update getquestion and startgame functions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,8 +1,14 @@
 
-document.getElementById("start").addEventListener("click", getQuestion);
+document.getElementById("start").addEventListener("click", startGame);
 
 function getQuestion() {
+   document.getElementById("question-title").innerText = questionSet[0].title;
+   document.getElementById("choices").innerText = questionSet[0].choices;
+}
+
+function startGame() {
     document.getElementById("start-screen").setAttribute("class", "hide");
-    document.getElementById("questions").setAttribute("class", "")
+    document.getElementById("questions").setAttribute("class", "start");
+    getQuestion();
 }
 
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,8 +1,8 @@
-var questions = [
+var questionSet = [
     {
-        title: "question1",
-        choices: ["a1", "a2", "a3"],
-        correctAnswer: "correct",
+        title: "Arrays in JavaScript can be used to store _____",
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        correctAnswer: "All of the above",
     },
     {
         title: "question2",
