1743037b777767cacf525b7940261f248993c756
Liam Cottrell <lemcottrell@gmail.com>
2023-12-05 13:03:00 +0000

    change to queryselector

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,13 +2,13 @@
 document.getElementById("start").addEventListener("click", startGame);
 
 function getQuestion() {
-   document.getElementById("question-title").innerText = questionSet[0].title;
-   document.getElementById("choices").innerText = questionSet[0].choices[0];
+   document.querySelector("#question-title").innerText = questionSet[0].title;
+   
 }
 
 function startGame() {
-    document.getElementById("start-screen").setAttribute("class", "hide");
-    document.getElementById("questions").setAttribute("class", "start");
+    document.querySelector("#start-screen").setAttribute("class", "hide");
+    document.querySelector("#questions").setAttribute("class", "start");
     getQuestion();
 }
 
